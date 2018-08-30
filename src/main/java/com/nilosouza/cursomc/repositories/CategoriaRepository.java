dca330de775b355a2bae3d5a655cd6666e39fefc
Nilo Souza <nilo.contatos@gmail.com>
2018-08-29 21:14:21 -0300

    Erro ao executar metodo find

--- src/main/java/com/nilosouza/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/nilosouza/cursomc/repositories/CategoriaRepository.java
@@ -1,5 +1,6 @@
 package com.nilosouza.cursomc.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.nilosouza.cursomc.domain.Categoria;
---
+++ src/main/java/com/nilosouza/cursomc/services/CategoriaService.java
@@ -0,0 +1,25 @@
+package com.nilosouza.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nilosouza.cursomc.domain.Categoria;
+import com.nilosouza.cursomc.repositories.CategoriaRepository;
+
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	public CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);		
+		return obj.orElse(null);
+				
+	}
+
+}
